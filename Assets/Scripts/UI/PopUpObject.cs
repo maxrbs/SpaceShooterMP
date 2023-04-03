871cd091de93d64ba0467b1a069dd4036c9cc5e4
maxrbs <maksern.erb@gmail.com>
2023-04-03 10:11:09 +0500

    Animated PopUp object added

---
+++ Assets/Scripts/UI/PopUpObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PopUpObject : MonoBehaviour
+{
+    private Animator animatorComponent;
+    private bool isActive;
+
+    private void Start()
+    {
+        isActive = false;
+        animatorComponent = GetComponent<Animator>();
+    }
+
+    public void ShowPopUp()
+    {
+        if (isActive) return;
+
+        animatorComponent.Play("Show");
+        isActive = true;
+    }
+
+    public void ClosePopUp(float delayInSeconds)
+    {
+        StartCoroutine(CloseWithDelay(delayInSeconds));
+    }
+
+    public void ClosePopUp()
+    {
+        if (!isActive) return;
+
+        isActive = false;
+        animatorComponent.Play("Close");
+    }
+
+    private IEnumerator CloseWithDelay(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        ClosePopUp();
+    }
+
+}
