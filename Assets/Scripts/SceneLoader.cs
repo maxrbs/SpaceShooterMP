d9095c67b4ecdad0926ca93025c79944aea2b55c
maxrbs <maksern.erb@gmail.com>
2023-04-04 02:05:45 +0500

    Some little fixes

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,6 +8,12 @@ public class SceneLoader : MonoBehaviour
     private Animator splashScreenAnimator;
     private string nextSceneName;
 
+    private void Awake()
+    {
+        splashScreenAnimator = GetComponent<Animator>();
+        BrightenScreen();
+    }
+
     public void LoadScene(string sceneName)
     {
         nextSceneName = sceneName;
@@ -24,14 +30,13 @@ public class SceneLoader : MonoBehaviour
         splashScreenAnimator.Play("Hide");
     }
 
-    private void Awake()
-    {
-        splashScreenAnimator = GetComponent<Animator>();
-        BrightenScreen();
-    }
-
     private void StartLoadingScene()
     {
         SceneManager.LoadScene(nextSceneName);
+    }
+
+    public void LoadExitGame()
+    {
+        Application.Quit();
     }
 }
